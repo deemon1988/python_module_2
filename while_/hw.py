69ca60b70ef54acc01f68b70188df9f9631faa35
deemon1988 <dmn72835@gmail.com>
2024-10-12 13:22:44 +0300

    while - homework (complete)

--- while_/hw.py
+++ while_/hw.py
@@ -1,18 +1,17 @@
 # Домашняя работа по уроку "Стиль кода часть II. Цикл While"
 
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-
 new_list = []
-
 index = 0
 
-while len(my_list) != 0:
+while index < len(my_list):
     num = my_list[index]
     if num > 0:
         new_list.append(num)
         index += 1
-        print(new_list)
+        print(num)
     elif num < 0:
         break
     else:
+        index += 1
         continue
