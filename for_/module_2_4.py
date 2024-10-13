4194ea49ed50ad5196ede4f9142c9bb7e7dad3b1
deemon1988 <dmn72835@gmail.com>
2024-10-13 12:51:46 +0300

    for second commit

--- for_/module_2_4.py
+++ for_/module_2_4.py
@@ -2,21 +2,25 @@ from operator import index
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-left = 1
-right = len(numbers) - 1
+
 prime = []
 not_prime = []
 list_ = []
-for i in range(numbers[left], numbers[right]):
+for i in numbers:#range(numbers[left], numbers[right]):
+    if i == 1:
+        print("Excluded value:", i)
+        continue
     list_.append(i)
-    for j in range(list_[0], list_[-1]):
-        if i % j == 0:
+    for j in list_: #range(list_[0], list_[-1]):
+        if i % j != 0 or i == j:
+            if i not in prime:      # поставить перед проверкой деления
+                prime.append(i)
+            #continue
+        else:
             not_prime.append(i)
             break
-        else:
-            prime.append(i)
-            break
 
-
-print(prime)
-print(not_prime)
+# Простое число имеет только два делителя на 1 и само себя
+# Не простое число имеет больше 2х делителей, например 4 - 4:1, 4:4, 4:2
+print("Primes:" ,prime)
+print("Not Primes:" ,not_prime)
