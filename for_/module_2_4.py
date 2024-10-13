4cf53a5346f915cabd62e0b308b30f890ad38ebb
deemon1988 <dmn72835@gmail.com>
2024-10-13 15:36:33 +0300

    Prime numbers - complete variant

--- for_/module_2_4.py
+++ for_/module_2_4.py
@@ -1,33 +1,26 @@
-from operator import index
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-
 prime = []
 not_prime = []
-list_ = []
-for i in numbers:#range(numbers[left], numbers[right]):
+compare_list = []
+for i in numbers:
     if i == 1:
         print("Excluded value:", i)
         continue
-    list_.append(i)
-    for j in list_: #range(list_[0], list_[-1]):
-        while j in list_:
-            if i % j == 0 and i != j:
-                if i not in not_prime:      # поставить перед проверкой деления
-                    not_prime.append(i)
-                    break
-                else:
-                    break
-            elif i == j:    # если равно добавлять в prime
-                break
-            else: break
-            #continue
-    if i not in prime and i not in not_prime:
-        prime.append(i)
+    compare_list.append(i)
+    for j in compare_list:
+        if i % j == 0 and i != j:
+            not_prime.append(i)
+            break
+        elif i == j:
+            prime.append(i)
+            break
+        else:
+            continue
 
+print("Primes:" ,prime)
+print("Not Primes:" ,not_prime)
 
 # Простое число имеет только два делителя на 1 и само себя
 # Не простое число имеет больше 2х делителей, например 4 - 4:1, 4:4, 4:2
-print("Primes:" ,prime)
-print("Not Primes:" ,not_prime)
