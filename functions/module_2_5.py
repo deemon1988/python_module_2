595ccdfedc86ebe48d0616613aedd5dfc926e4bc
deemon1988 <dmn72835@gmail.com>
2024-10-15 11:56:24 +0300

    Домашняя работа по уроку "Функции в Python.Функция с параметром"

---
+++ functions/module_2_5.py
@@ -0,0 +1,18 @@
+# Домашняя работа по уроку "Функции в Python.Функция с параметром"
+
+def get_matrix(n, m ,value)->list:
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+result_1 = get_matrix(3,3,10)
+result_2 = get_matrix(5,5, 35)
+result_3 = get_matrix(3,2, 15)
+
+print(result_1)
+print(result_2)
+print(result_3)
