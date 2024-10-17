6c363dfcddf72856a2495b411b7e4f2990bd21cd
deemon1988 <dmn72835@gmail.com>
2024-10-17 13:20:58 +0300

    new example1 - Задание "Слишком древний шифр"

--- additional_practical/hw_completed.py
+++ additional_practical/hw_completed.py
@@ -1,33 +1,32 @@
 import random
 
-from functions.module_2_5 import result_1
-
 numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 n = random.choice(numbers)
 
 
 def find_pairs(rand_number) -> tuple:
     result = []
+    count = 0
     for i in range(1, 21):  # если i == n, то summ > n -> n % summ != 0
+        count += 1
         for j in range(i + 1, 21):  # если j == n, то summ > n -> n % summ != 0
+            count += 1
             summ = i + j
             if rand_number % summ == 0:
                 result.append((i, j), )
     tuple_ = (*result,)
+    print(count)
     return tuple_
 
+print(find_pairs(3))
+area = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
+for i in area:
+    print(*i)
 
 string = ''
 for a in numbers:
     string += str(a)
 print(string)
-
-find_pairs(5)
-print("for 5:", find_pairs(5))
-
-area = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
-for i in area:
-    print(*i)
 
 
 def print_numbers(args):
@@ -36,20 +35,21 @@ def print_numbers(args):
         str1 += str(*j)
     print("Print numbers:", str1)
 
-
 list_ = [[123], [124124], [1515]]
-
 print_numbers(list_)
 
 
 def find_pairs1(rand_number) -> tuple:
     result = []
     is_divisor = True
+    count = 0
     while is_divisor:
         for i in range(1, 21):  # если i == n, то summ > n -> n % summ != 0
+            count += 1
             if i == rand_number:
                 break
             for j in range(i + 1, 21):  # если j == n, то summ > n -> n % summ != 0
+                count += 1
                 if j == rand_number:
                     break
                 summ = i + j
@@ -61,7 +61,8 @@ def find_pairs1(rand_number) -> tuple:
             if not is_divisor:
                 break
     tuple_ = (*result,)
+    print(count)
     return tuple_
 
 
-print(find_pairs1(5))
+print(find_pairs1(3))
